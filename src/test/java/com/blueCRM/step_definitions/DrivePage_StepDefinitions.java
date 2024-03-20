1529b9f05526f5e9096ab83251c86d9dd9ea41c5
Ebal2023 <es.bal@outlook.com>
2024-03-20 18:00:27 -0500

    implemented steps for Drive page acceptance criteria

--- src/test/java/com/blueCRM/step_definitions/DrivePage_StepDefinitions.java
+++ src/test/java/com/blueCRM/step_definitions/DrivePage_StepDefinitions.java
@@ -1,23 +1,33 @@
 package com.blueCRM.step_definitions;
 
 import com.blueCRM.pages.ActivityStreamPage;
+import com.blueCRM.pages.DrivePage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
 public class DrivePage_StepDefinitions {
 
     ActivityStreamPage activityStreamPage = new ActivityStreamPage();
+    DrivePage drivePage = new DrivePage();
 
     @Given("the user clicks on the {string} option")
-    public void the_user_clicks_on_the_option(String string) {
+    public void the_user_clicks_on_the_option(String drive) {
 
         //specifically navigates to the Drive page
-        activityStreamPage.navigateToModule(string);
+        activityStreamPage.navigateToModule(drive);
     }
 
 
     @Then("the user sees the modules")
     public void the_user_sees_the_modules(io.cucumber.datatable.DataTable dataTable) {
         //need to locate each module seperately.
+
+        Assert.assertTrue(drivePage.myDriveModule.isDisplayed());
+        Assert.assertTrue(drivePage.allDocumentsModule.isDisplayed());
+        Assert.assertTrue(drivePage.companyDriveModule.isDisplayed());
+        Assert.assertTrue(drivePage.salesAndMarketingModule.isDisplayed());
+        Assert.assertTrue(drivePage.topManagementDocumentsModule.isDisplayed());
+        Assert.assertTrue(drivePage.driveCleanupModule.isDisplayed());
     }
 }
