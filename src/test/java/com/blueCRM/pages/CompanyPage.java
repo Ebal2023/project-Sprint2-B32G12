65671eaae8e811b746791083ec43115ff5b417f1
Amirullahomari <amirullahomari44@gmail.com>
2024-03-21 08:36:24 -0800

    Created CompanyPage

--- src/test/java/com/blueCRM/pages/CompanyPage.java
+++ src/test/java/com/blueCRM/pages/CompanyPage.java
@@ -4,6 +4,10 @@ import com.blueCRM.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.w3c.dom.css.CSSPageRule;
+
+import javax.management.modelmbean.ModelMBeanInfoSupport;
+import java.util.List;
 
 public class CompanyPage {
 
@@ -17,4 +21,10 @@ public class CompanyPage {
     @FindBy(partialLinkText = "Company")
     public WebElement companyLink;
 
+
+    @FindBy(css = ".main-buttons-item-link" )
+    public List<WebElement>  modulesActualList;
+
 }
+
+
