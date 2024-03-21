0c85489187729d287e956fa1bf306451608d991a
Amirullahomari <amirullahomari44@gmail.com>
2024-03-21 08:36:42 -0800

    Created CompanyStepDefs

--- src/test/java/com/blueCRM/step_definitions/CompanyStepDefs.java
+++ src/test/java/com/blueCRM/step_definitions/CompanyStepDefs.java
@@ -1,7 +1,14 @@
 package com.blueCRM.step_definitions;
 
 import com.blueCRM.pages.CompanyPage;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class CompanyStepDefs {
 
@@ -17,6 +24,28 @@ public class CompanyStepDefs {
 
 
     }
+
+    @Then("user sees the following modules")
+    public void user_sees_the_following_modules(List<String> modulesExpectedNames) {
+
+        List<String> actualModules = new ArrayList<>();
+
+        for (WebElement each : companyPage.modulesActualList) {
+            actualModules.add(each.getText());
+
+
+        }
+
+
+
+        Assert.assertEquals(modulesExpectedNames,actualModules);
+
+    }
+
+
+
+
+
 
 
 
